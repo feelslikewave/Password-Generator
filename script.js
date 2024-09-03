8c5f7675cd5cf9cbcfc91054d534a97128ee61bf
feelslikewave <acharya.tarang.103@gmail.com>
2024-09-03 21:43:44 +0530

    updated

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const length = 12;
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const number = '0123456789';
-const symbols = '@#$%^&*()_+~|}{[]></?-=';
-const allChars = 
+const symbols = '@#$%^&*_+~|/?-=';
+const allChars = upperCase + lowerCase + number + symbols;
 
 function createPassword() {
   let password = '';
@@ -14,6 +14,12 @@ function createPassword() {
   password += symbols[Math.floor(Math.random()*symbols.length)];   
 
   while(length>password.length){
-
+    password += allChars[Math.floor(Math.random()*allChars.length)];  
   }
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand('copy');
 }
